147d71b3d1638433983970ac82fff4ce2a2dac58
JorgeTordecilla <68673293+JorgeTordecilla@users.noreply.github.com>
2021-03-18 19:32:22 -0500

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,1 @@
-// Javascript
-$(function($) {
-    "use strict";
 
-	/* SMOOTH SCROLL */
-    $('.smooth').click(function(event){		
-        event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 70);
-    });
-
-});
